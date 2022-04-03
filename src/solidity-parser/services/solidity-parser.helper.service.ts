7ba7229792d13bdbb58bb19ece189880dda05dee
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2022-04-03 17:24:17 +0400

    - implement solidity-parser module helper service

---
+++ src/solidity-parser/services/solidity-parser.helper.service.ts
@@ -0,0 +1,5 @@
+export class SolidityParserHelperService {
+  public SOLIDITY_TYPE = 'SourceUnit';
+  public SOLIDITY_IMPORT_DIRECTIVE = 'ImportDirective';
+  public SOLIDITY_CONTRACT_DEFINITION = 'ContractDefinition';
+}
--- src/solidity-parser/solidity-parser.module.ts
+++ src/solidity-parser/solidity-parser.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SolidityParserService } from './solidity-parser.service';
 import { SolidityParserController } from './solidity-parser.controller';
+import { SolidityParserHelperService } from './services/solidity-parser.helper.service';
 
 @Module({
   controllers: [SolidityParserController],
-  providers: [SolidityParserService],
+  providers: [SolidityParserHelperService, SolidityParserService],
 })
 export class SolidityParserModule {}
