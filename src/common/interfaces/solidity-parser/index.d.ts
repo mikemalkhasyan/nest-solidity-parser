2da948661e576babb67805e0257a86f527ec7115
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2022-04-03 17:25:02 +0400

    - add solidity parser module necessary interfaces

--- src/common/interfaces/solidity-parser/index.d.ts
+++ src/common/interfaces/solidity-parser/index.d.ts
@@ -2,3 +2,35 @@ export interface ISolidityParserResponse {
   imports: Array<string>;
   contracts: Array<string>;
 }
+
+export interface IPathLiteral {
+  type: string;
+  value: string;
+  parts: Array<string>;
+  isUnicode: Array<boolean>;
+}
+
+export interface ISubNodes {
+  type: string;
+  variable: Array<any>;
+  initialValue: null;
+}
+
+export interface ISolidityParseChildren {
+  type: string;
+  name?: string;
+  path?: string;
+  pathLiteral?: IPathLiteral;
+  unitAlias?: null;
+  unitAliasIdentifier?: null;
+  symbolAliases?: null;
+  symbolAliasesIdentifiers?: null;
+  baseContracts?: Array<any>;
+  subNodes?: Array<ISubNodes>;
+  kind?: string;
+}
+
+export interface ISolidityParseBody {
+  type: string;
+  children: Array<ISolidityParseChildren>;
+}
